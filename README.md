dce6f97329748775687fda15bac59e329d7574ad
Hyunwoo Song <chzhqk1994@gmail.com>
2023-05-04 00:32:59 +0900

    vscode configuration, YOLOv5s ONNX model loaded

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # torch_cpp_test
+
+1. ONNX DNN inference bug: (-215:Assertion failed) inputs.size() in function 'getMemoryShapes'
+
+  1) onnx 모델 load 과정에서 위 와 같은 에러가 발생한다면 torch 1.12 가 아닌 다른 버전을 사용해서 onnx 모델을 만들면 해결된다 (ex torch 1.10 or 1.11)
+  2) 또는 export.py 에서 아래 내용을 False 로 변경
+    do_constant_folding=True,  # WARNING: DNN inference with torch>=1.12 may require do_constant_folding=False 
+
+
+2. OpenCV(4.4.0) onnx_importer.cpp:1410: error: (-2:Unspecified error) in function 'void cv::dnn::dnn4_v20200609::ONNXImporter::populateNet(cv::dnn::dnn4_v20200609::Net)'
+
+  위 와 같은 에러는 Upsampling 레이어에 의한 것이라고 하는데 opencv 버전을 4.5+ 로 업그레이드 하면 버그가 사라짐
+  참조 : https://codeantenna.com/a/ofKLdfMAba
--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
-#include <iostream>  
-#include "opencv2/opencv.hpp"
-  
-using namespace cv;  
-using namespace std;  
-  
-int main() { 
-    cout << "Hello Opencv!" << endl;  
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace cv::dnn;
+
+const float INPUT_WIDTH = 640;
+const float INPUT_HEIGHT = 640;
+
+const float SCORE_THRESHOLD = 0.5;
+const float CONFIDENCE_THRESHOLD = 0.45;
+const float NMS_THRESHOLD = 0.45;
+
+const float FONT_SCALE = 0.7;
+const int FONT_FACE = FONT_HERSHEY_SIMPLEX;
+const int THICKNESS = 1;
+
+Scalar BLACK = Scalar(0, 0, 0);
+Scalar RED = Scalar(0, 0, 255);
+Scalar GREEN = Scalar(0, 255, 0);
+Scalar BLUE = Scalar(255, 0, 0);
+
+vector<Mat> model_input(Mat input_image, Net model) {
+    Mat blob;
+
+    // Net에 입력되는 형태는 blob 형태로 변경 해줘야 한다
+    // blob : opencv 에서 Mat 타입의 4차원 텐서 (NCHW)
+    blobFromImage(input_image, blob, 1/255, Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false);
+
+    // model 에 blob 형태의 데이터를 넣어줌
+    model.setInput(blob);
+
+    // blob 을 받은 model 을 실행 시켜줌(forward propagation)
+    vector<Mat> outputs;
+    model.forward(outputs, model.getUnconnectedOutLayersNames());
+
+    return outputs;
+}
+
+int main(int, char **) {
+    vector<string> class_list;
+    String string;
+    fstream ifs("coco_labels.txt");
+    while (getline(ifs, string))
+        class_list.push_back(string);
+
+    String image_path = "lenna.png";
+    Mat image = imread(image_path);
+
+    // Load model
+    Net model;
+    model = readNet("yolov5s.onnx");
+
+    vector<Mat> detections;
+    detections = model_input(image, model);  // inference
+
+
     return 0;
 }
